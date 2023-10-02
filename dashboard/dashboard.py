4783c6f0db6a364c6c06c950c3918ed5b3d4c435
Indra Cahya Ramdani <37335233+indracahyaramdani@users.noreply.github.com>
2023-10-02 23:10:58 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -89,7 +89,7 @@ with col3:
     st.metric(label="Total Record", value=total_record)
  
 
-st.subheader('Count of bikes during different Season',divider='black')
+st.subheader('Count of bikes during different Season')
 season(main_df)
 
 #st.subheader('Count of bikes during different months',divider='rainbow')
